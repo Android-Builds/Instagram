90cea3555185f98ed311a317766fd5d2f7107507
Debjit Chakraborty <zxyron0@gmail.com>
2020-08-15 23:07:05 +0530

    Long press to shrink widget & click to show story

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -42,21 +44,88 @@ class _MyHomePageState extends State<MyHomePage> {
                   scrollDirection: Axis.horizontal,
                   itemCount: 20,
                   itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: GestureDetector(
-                        onLongPress: () {},
-                        child: CircleAvatar(
-                          radius: 35.0,
-                          child: Icon(Icons.person),
-                        ),
-                      ),
-                    );
+                    return StoryWidget();
                   }),
             )
           ],
         ),
       ),
     );
+  }
+}
+
+class StoryWidget extends StatefulWidget {
+  const StoryWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _StoryWidgetState createState() => _StoryWidgetState();
+}
+
+class _StoryWidgetState extends State<StoryWidget> {
+  double radius = 30.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Container(
+        padding: EdgeInsets.all(2.0),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.blue,
+          ),
+          shape: BoxShape.circle,
+        ),
+        child: GestureDetector(
+          onLongPressStart: (details) {
+            setState(() {
+              radius = 25.0;
+            });
+          },
+          onLongPressEnd: (details) {
+            setState(() {
+              radius = 30.0;
+            });
+          },
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => StoryPage()));
+          },
+          child: Container(
+            decoration: ShapeDecoration(shape: CircleBorder()),
+            child: CircleAvatar(
+              radius: radius,
+              child: Icon(Icons.person),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class StoryPage extends StatefulWidget {
+  @override
+  _StoryPageState createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 5), () {
+      Navigator.pop(context);
+    });
+  }
+
+  exit() {
+    Navigator.pop(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
