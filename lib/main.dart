d393b2012f245a5d2340e43da32a02d3c2b9e59f
Debjit Chakraborty <zxyron0@gmail.com>
2020-08-15 23:23:48 +0530

    Add themes

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,33 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: 'Instagram',
+      theme: ThemeData.light().copyWith(
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          elevation: 0.0,
+          color: Colors.white,
+          iconTheme: IconThemeData(color: Colors.black),
+          textTheme: TextTheme(
+            headline6: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0),
+          ),
+        ),
       ),
-      //darkTheme: ThemeData.dark(),
+      darkTheme: ThemeData.dark().copyWith(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        canvasColor: Colors.black,
+        scaffoldBackgroundColor: Colors.black,
+        bottomSheetTheme: BottomSheetThemeData(
+          backgroundColor: Colors.grey[900],
+        ),
+        appBarTheme: AppBarTheme(color: Colors.black),
+        popupMenuTheme: PopupMenuThemeData(color: Colors.black),
+        dialogBackgroundColor: Colors.black,
+      ),
       home: MyHomePage(),
     );
   }
