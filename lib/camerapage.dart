012d556c9755dd601db46a912e18d42ad12a7e86
Debjit Chakraborty <zxyron0@gmail.com>
2020-08-23 12:20:41 +0530

    Create story button [2] Wasted literally 2 hours for the previous solution when this one is so simple and a matter of 1 minute

--- lib/camerapage.dart
+++ lib/camerapage.dart
@@ -50,29 +50,42 @@ class _CameraAppState extends State<CameraApp> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(60),
-                  child: Stack(
-                    children: [
-                      Cutout(
-                        color: Colors.white,
-                        child: Icon(
-                          Icons.brightness_1,
-                          size: 80.0,
-                        ),
-                      ),
-                      Positioned(
-                        left: 5.0,
-                        top: 5.0,
-                        child: Icon(
-                          Icons.brightness_1,
-                          size: 70.0,
-                          color: Colors.white,
-                        ),
-                      )
-                    ],
+                OutlineButton(
+                  onPressed: () {},
+                  shape: new CircleBorder(),
+                  borderSide: BorderSide(color: Colors.white, width: 3.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(0),
+                    child: Icon(
+                      Icons.brightness_1,
+                      color: Colors.white,
+                      size: 80.0,
+                    ),
                   ),
                 ),
+                // ClipRRect(
+                //   borderRadius: BorderRadius.circular(60),
+                //   child: Stack(
+                //     children: [
+                //       Cutout(
+                //         color: Colors.white,
+                //         child: Icon(
+                //           Icons.brightness_1,
+                //           size: 80.0,
+                //         ),
+                //       ),
+                //       Positioned(
+                //         left: 5.0,
+                //         top: 5.0,
+                //         child: Icon(
+                //           Icons.brightness_1,
+                //           size: 70.0,
+                //           color: Colors.white,
+                //         ),
+                //       )
+                //     ],
+                //   ),
+                // ),
                 SizedBox(height: 50.0)
               ],
             ),
